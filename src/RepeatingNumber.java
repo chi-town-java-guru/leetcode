1e0f614ad61f3bf449ff4e7a8c558685740a7804
guru <gurunath.sugavanam@gmail.com>
2022-08-25 15:45:34 +0530

    sort by frequency

--- src/RepeatingNumber.java
+++ src/RepeatingNumber.java
@@ -5,13 +5,14 @@ public class RepeatingNumber {
     //Find first repeating number
     public static void main(String[] args) {
        // int[] input = {2, 5, 1, 2, 3, 5, 1, 2, 4};//must return 2
-        int[] input = {2, 3,4,5,2};//must return undefined
+        int[] input = {2, 3,4,5,23};//must return undefined
 
         System.out.println(findFirstRepeatingNumber(input));
 
     }
 
     private static String findFirstRepeatingNumber(int[] input) {
+
         Set<Integer> nums = new HashSet<>();
         for (int j : input) {
             if (nums.contains(j)) {
