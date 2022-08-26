4de7b2a6bffd4eb0f399e98f1add46c3db0b1d37
guru <gurunath.sugavanam@gmail.com>
2022-08-26 05:31:01 +0530

    stream programming

--- src/com/buycoinsellcoin/PrimeNumber.java
+++ src/com/buycoinsellcoin/PrimeNumber.java
@@ -5,12 +5,15 @@ import static java.util.stream.IntStream.iterate;
 public class PrimeNumber {
 
     public static void main(String[] args) {
-        int input = 14;
-        System.out.println(input + " is a Prime Number: " + primeNumber(input));
+        // int input = 13;
+        iterate(2, i -> i < 100, i -> i + 1)
+                .filter(PrimeNumber::primeNumber)
+                .forEach(i -> System.out.println(i + " is a Prime Number: " + primeNumber(i)));
 
     }
 
     private static boolean primeNumber(int input) {
-        return iterate(2, i -> i <= Math.sqrt(input) && input % i == 0, i -> i + 1).noneMatch(i -> true);
+        return iterate(2, i -> i <= Math.sqrt(input), i -> i + 1)
+                .noneMatch(i -> input % i == 0);
     }
 }
